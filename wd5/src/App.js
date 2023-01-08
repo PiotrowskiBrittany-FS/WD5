5a22933f40e1716100635226c48af94b6ad67b41
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-01-07 19:13:23 -0500

    Testing api

--- wd5/src/App.js
+++ wd5/src/App.js
@@ -1,16 +1,26 @@
-import React, {Component} from 'react';
+// import React from 'react';
 
-class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <header>
-          <h1>Hello!</h1>
-        </header>
-      </div>
-    );
-  }
+function App(){
+  const apiKey = 'AIzaSyCzsyEhEjXpxHjNcFzG3Kuhj3WXWCNiBrw';
+  const search = 'Svekis';
+  const url = `https://www.googleapis.com/youtube/v3/search/?part=snippet&key=${apiKey}&q=${search}&maxResults=20`;
+
+  console.log(url);
+  const output = document.querySelector('.output');
+  const button = document.createElement('button');
+  button.textContent = 'Get Data';
+  output.append(button);
+
+  button.addEventListener('click', (e)=> {
+    fetch(url).then(rep=>rep.json()).then((data)=>{
+      console.log(data.items);
+    })
+  })
 
 }
+
+
+
+
 
 export default App;
