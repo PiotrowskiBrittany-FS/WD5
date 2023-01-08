e1cfcaca0ab48cedc7100f312628c1103eec3268
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-01-08 10:59:17 -0500

    resetting to start over due to several errors

--- wd5/src/App.js
+++ wd5/src/App.js
@@ -1,21 +1,7 @@
-// import React from 'react';
+//import React from 'react';
 
 function App(){
-  const apiKey = 'AIzaSyCzsyEhEjXpxHjNcFzG3Kuhj3WXWCNiBrw';
-  const search = 'Svekis';
-  const url = `https://www.googleapis.com/youtube/v3/search/?part=snippet&key=${apiKey}&q=${search}&maxResults=20`;
-
-  console.log(url);
-  const output = document.querySelector('.output');
-  const button = document.createElement('button');
-  button.textContent = 'Get Data';
-  output.append(button);
-
-  button.addEventListener('click', (e)=> {
-    fetch(url).then(rep=>rep.json()).then((data)=>{
-      console.log(data.items);
-    })
-  })
+  
 
 }
 
