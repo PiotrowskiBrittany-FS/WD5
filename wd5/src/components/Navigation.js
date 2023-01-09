8b208fd8fb4266ad6b89c795ba85e05a64c3c244
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-01-08 21:38:52 -0500

    name change

--- wd5/src/components/Navigation.js
+++ wd5/src/components/Navigation.js
@@ -4,7 +4,7 @@ const Navigation = ()=>{
     return(
         <nav>
             <div className="nav-wrapper">
-                <h1><a href="#" className="logo">Popcorn Up</a></h1>
+                <h1><a href="#" className="logo">Popcorner</a></h1>
             </div>
         </nav>
     )
