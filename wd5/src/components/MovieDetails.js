7c07d3cbb5c5d7c19dbb3eaf4bea5fabaa63fc33
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-01-08 19:54:09 -0500

    Changed image placeholder to an appropriate image

--- wd5/src/components/MovieDetails.js
+++ wd5/src/components/MovieDetails.js
@@ -9,7 +9,7 @@ const MovieDetails = (props)=>{
             </div>
             <div className="image">
                 {
-                    props.currentMovie.poster_path === null ? <img src={`https://s3-ap-southeast1.amazonaws.com/upcode/static/default-image.jpg`} alt="card image" style={{width: "100%", height: "360"}}/> : <img src={`http://image.tmdb.org/t/p/w185${props.currentMovie.poster_path}`} alt="card image" style={{width: "100%", height: "360"}}/>
+                    props.currentMovie.poster_path === null ? <img src={`https://www.prokerala.com/movies/assets/img/no-poster-available.jpg`} alt="card image" style={{width: "100%", height: "360"}}/> : <img src={`http://image.tmdb.org/t/p/w185${props.currentMovie.poster_path}`} alt="card image" style={{width: "100%", height: "360"}}/>
                 }
             </div>
             <div className="details">
