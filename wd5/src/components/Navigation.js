ff53c3b81a22bf43d473e0e1dbede6b30ad15f59
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-01-24 23:17:33 -0500

    Added Router back for testing

--- wd5/src/components/Navigation.js
+++ wd5/src/components/Navigation.js
@@ -1,21 +1,22 @@
 import React from "react";
-//import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const Navigation = ()=>{
     return(
         <nav>
             <div>
                 <h1>Popcorner</h1>
-                {/* <Link style={styles.links} to='/popularmovies'>Popular Movies</Link> */}
+                <Link style={styles.links} to='/popularmovies'>Popular Movies</Link>
             </div>
         </nav>
     )
 }
 export default Navigation;
 
-// const styles ={
-//     links:{
-//         display: 'flex',
-//         paddingRight: '2rem'
-//     }
-// }
+const styles ={
+    links:{
+        display: 'flex',
+        paddingRight: '2rem',
+        color: '#000'
+    }
+}
