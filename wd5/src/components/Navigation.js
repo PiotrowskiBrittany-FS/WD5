a29f10411dbf2ffbd21c4f54a114062b51f887f2
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-01-25 01:40:23 -0500

    Removed nav tag

--- wd5/src/components/Navigation.js
+++ wd5/src/components/Navigation.js
@@ -3,13 +3,11 @@ import {Link} from 'react-router-dom';
 
 const Navigation = ()=>{
     return(
-        <nav>
-            <div>
-                <Link to="/Home"> Home </Link>
-                <Link to="/SearchMovies">Search Movies</Link>
-                <Link style={styles.links} to='/PopularMovies'>Popular Movies</Link>
-            </div>
-        </nav>
+        <div>
+            <Link to="/Home"> Home </Link>
+            <Link to="/SearchMovies">Search Movies</Link>
+            <Link style={styles.links} to='/PopularMovies'>Popular Movies</Link>
+        </div>
     )
 }
 export default Navigation;
