67bd2e81b764919bd0046f031acc257809430777
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-01-25 19:54:07 -0500

    Fixed display for popular movies. Movies will now display once user selects Popular movies button

--- src/pages/PopularMovies.js
+++ src/pages/PopularMovies.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 
 import MovieList from '../components/MovieList';
-import Pagination from '../components/Pagination';
 import MovieDetails from '../components/MovieDetails';
 
 class PopularMovies extends Component{
@@ -15,29 +14,22 @@ class PopularMovies extends Component{
         this.apiKey = 'd90675c6ac13f9cd080926e402046ab1'
       }
 
-      handleSubmit = (e)=>{
-        e.preventDefault();
-          fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${this.apiKey}&language=en-US&page=1`)
+      render(){
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${this.apiKey}&language=en-US&page=1`)
           .then(data => data.json())
           .then(data => {
             console.log(data);
             this.setState({movies: [...data.results], totalMovies: data.total_results})
           })
-      } 
-
-      render(){
-        const numberPages = Math.floor(this.state.totalMovies / 20);
         return(
           <div style={styles.container}>
             <div style={styles.wrapper}>
+                <h2>Popular Movies</h2>
               {this.state.currentMovie == null ? 
                 <div style={styles.movies}>
                     <MovieList style={styles.list} viewDetails={this.viewDetails} movies={this.state.movies}/>
                 </div> : <MovieDetails style={styles.details} currentMovie={this.state.currentMovie} closeDetails={this.closeDetails}/>
               }
-              
-              {this.state.totalMovies > 20 && this.currentMovie == null ? <Pagination pages={numberPages} nextPage={this.nextPage} currentPage={this.state.currentPage}/> : ''}
-              {/* if movie results is more than 20, the pagination bar will show */}
             </div>
           </div>
         )
