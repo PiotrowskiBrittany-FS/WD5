ac88bf1ab8bc3f54ae336a2aa0276482b5120969
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-01-25 19:50:21 -0500

    added css to nav buttons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,10 @@
-
 import React from "react";
 import {Link} from 'react-router-dom';
 
 const Navigation = ()=>{
     return(
-        <div>
-            <Link style={styles.links} to="/Home"> Popcorner </Link>
+        <div style={styles.container}>
+            <Link to="/Home"> Popcorner </Link>
             <Link style={styles.links} to="/SearchMovies">Search Movies</Link>
             <Link style={styles.links} to='/PopularMovies'>Popular Movies</Link>
         </div>
@@ -14,9 +13,21 @@ const Navigation = ()=>{
 export default Navigation;
 
 const styles ={
-    links:{
+    container: {
         display: 'flex',
-        paddingRight: '2rem',
-        color: '#000'
-    }
+        justifyContent: 'space-between',
+    },
+    links:{
+        padding: '0.50rem',
+        color: '#000',
+        textDecoration: 'none',
+        borderStyle: 'solid',
+        borderWidth: '0px',
+        borderRadius: '10px',
+        cursor: 'pointer',
+        boxShadow: "2px 2px 5px #888",
+        backgroundColor: '#F2B705',
+        fontWeight: 'bold'
+    },
+
 }
