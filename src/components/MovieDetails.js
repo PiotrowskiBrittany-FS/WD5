ad725eaab20bc64961d3149e45f6fe279e891026
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-01-29 19:31:59 -0500

    Added Movie Rating

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -4,23 +4,25 @@ const MovieDetails = (props)=>{
     return(
         <div className="container">
             <div className="go-back" onClick={props.closeDetails}>
-                <span style={styles.back}>Go back</span>
+                    <span style={styles.back}>Go back</span>
             </div>
-            <div className="image">
-                {
-                    props.currentMovie.poster_path === null ? <img src={`https://www.prokerala.com/movies/assets/img/no-poster-available.jpg`} alt="card image" style={styles.images}/> : <img src={`http://image.tmdb.org/t/p/w185${props.currentMovie.poster_path}`} alt="card image" style={styles.images}/>
-                }
-            </div>
-            <div className="details" style={styles.detail}>
-                <p style={styles.info}>Title: </p>
-                <p>{props.currentMovie.title}</p>
-                <p style={styles.info}>Release Date: </p>
-                <p>{props.currentMovie.release_date.substring(5).split("-").concat(props.currentMovie.release_date.substring(0, 4)).join("/")}</p>
-                {/* Capture the date and use a substring of 5 to return up to the 5th index in order to flip the year to the back of the string */}
-                <p style={styles.info}>Runtime: </p>
-                <p>{props.currentMovie.runtime}</p>
-                <p style={styles.info}>About: </p>
-                <p>{props.currentMovie.overview}</p>
+            <div style={styles.container}>
+                <div className="image">
+                    {
+                        props.currentMovie.poster_path === null ? <img src={`https://www.prokerala.com/movies/assets/img/no-poster-available.jpg`} alt="card image" style={styles.images}/> : <img src={`http://image.tmdb.org/t/p/w185${props.currentMovie.poster_path}`} alt="card image" style={styles.images}/>
+                    }
+                </div>
+                <div className="details" style={styles.detail}>
+                    <p style={styles.info}>Title: </p>
+                    <p>{props.currentMovie.title}</p>
+                    <p style={styles.info}>Release Date: </p>
+                    <p>{props.currentMovie.release_date.substring(5).split("-").concat(props.currentMovie.release_date.substring(0, 4)).join("/")}</p>
+                    {/* Capture the date and use a substring of 5 to return up to the 5th index in order to flip the year to the back of the string */}
+                    <p style={styles.info}>Rating: </p>
+                    <p>{props.currentMovie.vote_average}</p>
+                    <p style={styles.info}>About: </p>
+                    <p>{props.currentMovie.overview}</p>
+                </div>
             </div>
         </div>
     )
@@ -31,7 +33,8 @@ export default MovieDetails;
 const styles ={
     container:{
         display: "flex",
-        justifyContent: "space-between"
+        justifyContent: "space-around",
+        flexWrap: 'wrap'
     },
     back: {
         borderStyle: "solid",
@@ -40,12 +43,17 @@ const styles ={
         cursor: "pointer",
         marginBottom: "2rem",
         padding: "0.5rem",
-        backgroundColor: "#F2B705"
+        backgroundColor: "#F2B705",
+        width: '10rem'
     },
     images:{
         paddingTop: "2rem",
-        width: "15rem",
-        height: "25rem"
+        width: "25rem",
+        height: "35rem"
+    },
+    detail:{
+        display: 'block',
+        width: '25rem'
     },
     info:{
         fontWeight: "bold",
